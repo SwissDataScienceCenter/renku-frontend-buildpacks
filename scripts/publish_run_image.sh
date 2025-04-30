645bd39d2047564d285a81f2bc7fa5e81065b75a
Tasko Olevski <olevski90@gmail.com>
2025-04-30 09:49:50 +0200

    feat: set default workdir in run image and cleanup extensions (#32)

---
+++ scripts/publish_run_image.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# The image name excluding the tag
+IMAGE=$1
+# If set to anything the image will be pushed to the image repository
+PUBLISH=$2
+
+TAG=$(git describe --exact-match --tags)
+if [[ -z $TAG ]]; then
+	TAG=$(git rev-parse HEAD | cut -c -7)
+fi
+
+echo "Building run image $IMAGE:$TAG"
+docker build -t "$IMAGE:$TAG" ./run-image/
+
+if [[ -n $PUBLISH ]]; then
+	echo "Found publish flag, will push the image to the repo"
+	docker push "$IMAGE:$TAG"
+fi
