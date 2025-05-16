ee3496e137b86817e401385349c86f4dbedef927
Tasko Olevski <olevski90@gmail.com>
2025-05-16 16:31:39 +0200

    fix: jupyterlab terminal work directory (#50)

---
+++ buildpacks/jupyterlab/bin/jupyterlab-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+JUPYTER_ENV_DIR=$1
+
+if [ -z "$JUPYTER_ENV_DIR" ]; then
+	echo "WARNING: Running with JUPYTER_ENV_DIR not set at all, this means python and jupyter executables are expected at /bin/."
+fi
+
+if [ -n "$RENKU_WORKING_DIR" ]; then
+	if [ ! -d "$RENKU_WORKING_DIR" ]; then
+		mkdir -p "$RENKU_WORKING_DIR"
+	fi
+	# This allows the terminal in jupyterlab to open in the working directory
+	cd "$RENKU_WORKING_DIR"
+	# The ServerApp.root_dir further below only changes the file browser location in the Jupyter UI
+fi
+
+"${JUPYTER_ENV_DIR}/bin/python" -E "${JUPYTER_ENV_DIR}/bin/jupyter-lab" \
+	--ip "${RENKU_SESSION_IP}" \
+	--port "${RENKU_SESSION_PORT}" \
+	--ServerApp.base_url "$RENKU_BASE_URL_PATH" \
+	--IdentityProvider.token "" \
+	--ServerApp.password "" \
+	--ServerApp.allow_remote_access true \
+	--ContentsManager.allow_hidden true \
+	--ServerApp.root_dir "${RENKU_WORKING_DIR}" \
+	--KernelSpecManager.ensure_native_kernel False
