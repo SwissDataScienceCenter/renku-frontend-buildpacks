1b906c43d8fbdc5b1eb029ba7cf06f8e14b4db2f
Salim Kayal <salim.kayal@idiap.ch>
2025-07-22 11:53:42 +0200

    fix: make tmux optional

--- buildpacks/ttyd/bin/ttyd-entrypoint.sh
+++ buildpacks/ttyd/bin/ttyd-entrypoint.sh
@@ -9,8 +9,10 @@ RENKU_SESSION_PORT="${RENKU_SESSION_PORT:-8888}"
 RENKU_WORKING_DIR="${RENKU_WORKING_DIR:-${HOME}}"
 RENKU_BASE_URL_PATH="${RENKU_BASE_URL_PATH:-/}"
 
+TTYD_CMD=$(which tmux >/dev/null && echo "tmux attach || tmux" || echo "bash")
+
 ttyd \
   --cwd "${RENKU_WORKING_DIR:-${HOME}}" \
   --port "${RENKU_SESSION_PORT:-8888}" \
   --base-path "${RENKU_BASE_URL_PATH:-/}" \
-  --writable sh -c 'tmux attach || tmux'
+  --writable sh -c "${TTYD_CMD}"
