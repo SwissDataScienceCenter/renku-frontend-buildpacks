42d390b257b89507c82e48a25b2ffcf32b003d3d
Salim Kayal <salim.kayal@idiap.ch>
2025-08-12 14:12:19 +0200

    fix: release on pr merge (#70)
    This PR introduces a new pull request-based workflow for our automated releases, aiming to improve control, reviewability, and traceability of the release process.

--- scripts/update_buildpack_versions.sh
+++ scripts/update_buildpack_versions.sh
@@ -3,7 +3,7 @@ RELEASE_VERSION="$1"
 FILE_PATH="$2"
 
 if [ -f "$FILE_PATH" ]; then
-  sed -i "s|^version = \"[^\"]*\"|version = ${RELEASE_VERSION}|g" "$FILE_PATH"
+  sed -i "s|^version = \"[^\"]*\"|version = \"${RELEASE_VERSION}\"|g" "$FILE_PATH"
   echo "Updated $FILE_PATH to version ${RELEASE_VERSION}"
 else
   echo "Error: File not found at ${FILE_PATH}. Skipping update." >&2
