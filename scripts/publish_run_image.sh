187d646b6c322f432359962b9dad1b09963342f9
Tasko Olevski <olevski90@gmail.com>
2025-12-12 01:00:03 +0100

    wip

--- scripts/publish_run_image.sh
+++ scripts/publish_run_image.sh
@@ -4,17 +4,17 @@ set -xeou pipefail
 # The image name excluding the tag
 IMAGE=$1
 # If set to anything the image will be pushed to the image repository
-PUBLISH=$2
+PUBLISH=${2:-}
 
-TAG=$(git describe --exact-match --tags)
+TAG=$(git describe --exact-match --tags || echo "")
 if [[ -z $TAG ]]; then
-	TAG=$(git rev-parse HEAD | cut -c -7)
+  TAG=$(git rev-parse HEAD | cut -c -7)
 fi
 
 echo "Building run image $IMAGE:$TAG"
 docker build -t "$IMAGE:$TAG" ./run-image/
 
 if [[ -n $PUBLISH ]]; then
-	echo "Found publish flag, will push the image to the repo"
-	docker push "$IMAGE:$TAG"
+  echo "Found publish flag, will push the image to the repo"
+  #docker push "$IMAGE:$TAG"
 fi
