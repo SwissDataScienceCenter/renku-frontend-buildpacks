ec25235a612cef1426ed3f94e72e19cf260e4dcb
Tasko Olevski <16360283+olevski@users.noreply.github.com>
2026-04-18 00:30:06 +0200

    squashme: remove old bash scripts

--- scripts/update_builder_versions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xeou pipefail
-BUILD_IMAGE="$1"
-BUILDER_FILE="$2"
-
-which dasel || (echo "dasel not found, please install it" && exit 100)
-
-dasel -i toml --root "build.image=\"$BUILD_IMAGE\"" <"$BUILDER_FILE" >"$BUILDER_FILE.tmp"
-mv "$BUILDER_FILE.tmp" "$BUILDER_FILE"
